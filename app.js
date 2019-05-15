6f6a2d7366d6804e2c2bc08809ac4a01881276d3
0ya-sh0 <ymthatte@gmail.com>
2019-05-15 15:49:13 +0530

    static serve

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res)=>{
-    res.send("hello world");
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 })
+
+app.use(express.static(path.join(__dirname, '/public/build')))
 
 module.exports = app;
