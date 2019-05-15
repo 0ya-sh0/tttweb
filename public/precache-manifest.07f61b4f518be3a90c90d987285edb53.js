a2bd50e5f8be9d5052247c3c49f3bd4659d55577
0ya-sh0 <ymthatte@gmail.com>
2019-05-15 15:44:06 +0530

    b1

---
+++ public/precache-manifest.07f61b4f518be3a90c90d987285edb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd7be794528a37df65117d8548c8192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee3e1b1374801a7c4f3",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ff73d13cad7be083af12",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "fee3e1b1374801a7c4f3",
+    "url": "/static/js/2.ef555117.chunk.js"
+  },
+  {
+    "revision": "ff73d13cad7be083af12",
+    "url": "/static/js/main.bbe06b06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
