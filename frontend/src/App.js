e1364220bc185dca18e6646b5d311d47cb1e912c
0ya-sh0 <ymthatte@gmail.com>
2019-05-15 20:29:32 +0530

    particle js added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,28 @@ import PageLocalGame from './components/PageLocalGame';
 import PageAiGame from './components/PageAiGame';
 import PageHome from './components/PageHome';
 import PageOnlineGame from './components/PageOnlineGame';
+import ParticlesContainer from './components/ParticlesContainer';
 
 function App() {
+  const style = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+  }
   return (
     <div className="App">
       <Router>
-          <Route exact path = "/app" component = {PageHome}></Route>
-          <Route path = "/app/local/" component = {PageLocalGame}></Route>
-          <Route path = "/app/ai/" component = {PageAiGame}></Route>
-          <Route path = "/app/online/" component = {PageOnlineGame}></Route>
+        <div style={style}>
+          <ParticlesContainer/>
+          <div style={style}>
+            <Route exact path = "/app" component = {PageHome}></Route>
+            <Route path = "/app/local/" component = {PageLocalGame}></Route>
+            <Route path = "/app/ai/" component = {PageAiGame}></Route>
+            <Route path = "/app/online/" component = {PageOnlineGame}></Route>
+          </div>
+        </div>
       </Router>
     </div>
   );
